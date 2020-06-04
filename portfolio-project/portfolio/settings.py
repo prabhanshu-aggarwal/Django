c0e8c38356a5e0fa28f5d6de2d1d675de519a193
Prabhanshu Aggarwal <prabhanshuaggarwal@gmail.com>
2020-06-05 00:02:58 +0530

    Migrate to update DB

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'job.apps.JobConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#To access the image
+MEDIA_URL= '/media/'
+
+
+                          
