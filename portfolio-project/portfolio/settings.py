b1d13e94edee8ce498aa032550ffda334b65ff51
Prabhanshu Aggarwal <prabhanshuaggarwal@gmail.com>
2020-06-05 20:45:08 +0530

    Added blog Model

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'job.apps.JobConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD'  : '123456789',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
