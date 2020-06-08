892fc4dedc9368e68b1fa241c86dc4261b4cdb50
Prabhanshu Aggarwal <prabhanshuaggarwal@gmail.com>
2020-06-08 23:56:24 +0530

    dded functionality of blog

--- portfolio-project/blog/views.py
+++ portfolio-project/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def allblogs(req):
+    return render(req, 'blog/allblogs.html')
--- portfolio-project/job/views.py
+++ portfolio-project/job/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .models import Job
 
 # Create your views here.
+
+
 def home(req):
     jobs=Job.objects #To get the data from the Db
     return render(req, 'job/home.html', {'jobs': jobs})
