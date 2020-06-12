dde4234f249be9875b2617ae0e10fd509ec35f69
Prabhanshu Aggarwal <prabhanshuaggarwal@gmail.com>
2020-06-12 22:55:55 +0530

    Added Sign Up

--- producthunt-project/accounts/urls.py
+++ producthunt-project/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.signup, name='signup'),
-    path('', views.login, name='login'),
-    path('', views.logout, name='logout'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
--- producthunt-project/producthunt/urls.py
+++ producthunt-project/producthunt/urls.py
@@ -20,5 +20,5 @@ from products import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('account/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
